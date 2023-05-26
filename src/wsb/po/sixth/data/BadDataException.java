b26730add24763f39f49ae1d1db71024e4e11a29
Piotr Bobiński <piotr.bobinski@wsb.warszawa.pl>
2023-05-26 10:49:50 +0200

    Przykłady - wykład 6 - pliki i wyjątki

---
+++ src/wsb/po/sixth/data/BadDataException.java
@@ -0,0 +1,15 @@
+package wsb.po.sixth.data;
+
+import java.io.IOException;
+
+/**
+   This class reports bad input data.
+*/
+public class BadDataException extends IOException
+{
+   public BadDataException() {}
+   public BadDataException(String message)
+   {
+      super(message);
+   }
+}
